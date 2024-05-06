753d17375a390f6b391554cb862ccedbe51bda2a
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2024-05-06 08:17:30 +0530

    Update JobSelection.cpp

--- JobSelection.cpp
+++ JobSelection.cpp
@@ -1,41 +1,58 @@
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Job {
 
-	char id;
-	int dead; 
+	char id; 
+	int dead;
 	int profit; 
+			
 };
 
-bool comparison(Job a, Job b)
-{
-	return (a.profit > b.profit);
-}
+struct jobProfit {
+	bool operator()(Job const& a, Job const& b)
+	{
+		return (a.profit < b.profit);
+	}
+};
 
 void printJobScheduling(Job arr[], int n)
 {
-	sort(arr, arr + n, comparison);
+	vector<Job> result;
+	sort(arr, arr + n,
+		[](Job a, Job b) { return a.dead < b.dead; });
 
-	int result[n];
-	bool slot[n]; 
-	for (int i = 0; i < n; i++)
-		slot[i] = false;
+	priority_queue<Job, vector<Job>, jobProfit> pq;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = min(n, arr[i].dead) - 1; j >= 0; j--) {
-			if (slot[j] == false) {
-				result[j] = i; 
-				slot[j] = true;
-				break;
-			}
+	for (int i = n - 1; i >= 0; i--) {
+		int slot_available;
+	
+		if (i == 0) {
+			slot_available = arr[i].dead;
+		}
+		else {
+			slot_available = arr[i].dead - arr[i - 1].dead;
+		}
+	
+		pq.push(arr[i]);
+	
+		while (slot_available > 0 && pq.size() > 0) {
+		
+			Job job = pq.top();
+			pq.pop();
+		
+			slot_available--;
+		
+			result.push_back(job);
 		}
 	}
 
-	for (int i = 0; i < n; i++)
-		if (slot[i])
-			cout << arr[result[i]].id << " ";
+	sort(result.begin(), result.end(),
+		[&](Job a, Job b) { return a.dead < b.dead; });
+
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i].id << ' ';
+	cout << endl;
 }
 
 int main()
